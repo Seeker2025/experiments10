b24326b94ca42141d4c2111e6d46503912a783f1
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-07-16 09:19:32 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@0064008f17373a537856ccace77e910dc5768d44 🚀

--- page-2.js
+++ page-2.js
@@ -1,4 +1,4 @@
-import"./assets/styles-BNA6e3xj.js";import{a as o}from"./assets/vendor-MDsUmn-G.js";console.log("exp 10 page 02");const r=document.querySelector(".gallery"),i="https://pixabay.com/api",n="51186890-e1c8ef6e5ef4b08950db17a2f";async function c(t){const a={params:{key:n,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0}};try{const e=await o.get(i,a);console.log(e.data.hits),s(e.data.hits)}catch{}}c("cat");function s(t){let a=null;a=t.map(e=>`
+import"./assets/styles-BNA6e3xj.js";import{a as o}from"./assets/vendor-MDsUmn-G.js";console.log("exp 10 page 02");const r=document.querySelector(".gallery"),i="https://pixabay.com/api/",n="51186890-e1c8ef6e5ef4b08950db17a2f";async function c(t){const a={params:{key:n,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0}};try{const e=await o.get(i,a);console.log(e.data.hits),s(e.data.hits)}catch{}}c("cat");function s(t){let a=null;a=t.map(e=>`
         <li class="gallery_item">
             <a href="${e.largeImageURL}">
                 <img src="${e.previewURL}" title="" />
