e840632a4fa850027e842e9a62516fda55e23f16
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-07-16 08:52:39 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@75c4832f6181756c465fd1438cf70625d5c28c60 🚀

--- page-2.js
+++ page-2.js
@@ -1,8 +1,8 @@
-import"./assets/styles-BNA6e3xj.js";import{a as o,S as i}from"./assets/vendor-B_f0CC_G.js";console.log("exp 10 page 02");const l=document.querySelector(".gallery"),r="51186890-e1c8ef6e5ef4b08950db17a2f";function n(a){o.get("https://pixabay.com/api",{params:{key:r,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(e=>{console.log(e.data.hits),c(e.data.hits),new i(".gallery_item a",{captionsData:"alt",captionDelay:200,animationSpeed:250,scaleImageToRatio:!0})}).catch(e=>console.log(e))}n("cat");function c(a){let e=null;e=a.map(t=>`
+import"./assets/styles-BNA6e3xj.js";import{a as o}from"./assets/vendor-MDsUmn-G.js";console.log("exp 10 page 02");const r=document.querySelector(".gallery"),i="https://pixabay.com/api",n="51186890-e1c8ef6e5ef4b08950db17a2f";async function c(t){const a={params:{key:n,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0}};try{const e=await o.get(i,a);console.log(e.data.hits),s(e.data.hits)}catch{}}c("cat");function s(t){let a=null;a=t.map(e=>`
         <li class="gallery_item">
-            <a href="${t.largeImageURL}">
-                <img src="${t.previewURL}" title="" />
+            <a href="${e.largeImageURL}">
+                <img src="${e.previewURL}" title="" />
             </a>
         </li>
-        `).join(),l.insertAdjacentHTML("beforeend",e)}
+        `).join(),r.insertAdjacentHTML("beforeend",a)}
 //# sourceMappingURL=page-2.js.map
