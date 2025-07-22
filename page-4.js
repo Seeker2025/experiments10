78e0a7ac5a9229da4c56ea3a757d21fece6c8dc2
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-07-22 14:48:43 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@e0875548c840771f211f762c5c497bc2eac30d51 🚀

---
+++ page-4.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BNA6e3xj.js";console.log("page 04");const n={a:"name",b:"surname"},o=JSON.stringify(n);console.log(o);console.log(typeof o);const s=JSON.parse(o);console.log(s);console.log(typeof s);
+//# sourceMappingURL=page-4.js.map
---
+++ page-5.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BNA6e3xj.js";console.log("page 05");
+//# sourceMappingURL=page-5.js.map
