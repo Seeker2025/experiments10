31ebc2dfbdfc98ecb2ffcc38fd806204496b89c0
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-07-26 11:53:18 +0300

    go on

--- src/js/page03.js
+++ src/js/page03.js
@@ -1,1 +1,46 @@
 console.log('page 03');
+////// Loader in HTML
+
+function getFruit(name){
+    const fruits = {
+        strawberry: 'berry01',
+        kiwi: 'berry02', 
+        apple: 'berry03',
+    }
+    return new Promise((resolve, reject) => 
+        setTimeout(() => {
+            resolve(fruits[name])
+           
+        
+        }, 1000)
+    );
+}
+
+try{
+    const apple = getFruit('apple');
+    console.log(apple);
+}catch(error){
+    console.log('mis');
+}
+
+// console.log(getFruit('apple'));
+
+// const ourPromise = new Promise((resolve, reject)=>{
+//     let number = Math.random() > .7;
+//    console.log(number);
+// setTimeout(()=>{
+//     // console.log('Hi!');
+//     if(number){
+//         resolve('Seccess!');
+//     }else{
+//         reject('Failure');
+//     }
+// }, 3000)
+    
+// })
+// console.log(ourPromise);
+
+// ourPromise.then(resolve => console.log('Данні з виконаного проміса:', resolve))
+//     .catch(reject => console.log('Помилка з виконаного проміса:', reject))
+// // .finally(()=> console.log('Проміс завершено!'))
+
--- src/js/page04.js
+++ src/js/page04.js
@@ -1,5 +1,5 @@
 console.log('page 04');
-////// JSON
+////// JSON, localStorage
 
 const string = {a: 'name', b: 'surname',}
 const string02 = JSON.stringify(string);
@@ -8,3 +8,28 @@ console.log(typeof(string02));
 const parseObj = JSON.parse(string02);
 console.log(parseObj);
 console.log(typeof(parseObj));
+
+const stringTwo = {name: 'Pete', girl: 'Rebecca',}
+localStorage.setItem('theOne', "text");
+const textOne = localStorage.getItem('theOne');
+console.log(textOne);
+
+console.log(JSON.stringify(stringTwo));
+localStorage.setItem('theThird', JSON.stringify(stringTwo));
+
+console.log(localStorage.getItem('theThird'));
+
+
+let theFourth = null;
+try{
+    boo
+    theFourth = JSON.parse(localStorage.getItem('theThird'));
+    console.log(JSON.parse(localStorage.getItem('theThird')));
+    localStorage.clear();
+    console.log(JSON.parse(localStorage.getItem('theThird'))); ////// null
+}catch(error){
+ console.log('hi');
+}
+
+
+
