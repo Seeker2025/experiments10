bef8b685040b4b74c2c1225bdf4e6b5a46c6308b
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-07-26 08:53:37 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@31ebc2dfbdfc98ecb2ffcc38fd806204496b89c0 🚀

--- page-3.js
+++ page-3.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BNA6e3xj.js";console.log("page 03");
+import"./assets/styles-BZUKjEjr.js";console.log("page 03");function t(e){const r={strawberry:"berry01",kiwi:"berry02",apple:"berry03"};return new Promise((o,s)=>setTimeout(()=>{o(r[e])},1e3))}try{const e=t("apple");console.log(e)}catch{console.log("mis")}
 //# sourceMappingURL=page-3.js.map
