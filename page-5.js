497586987a886051cb0bdb3ae2c3d8f36afca9a7
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-08-03 08:28:09 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@9bba1ab4b53e17d2e840871fc5f02895e9eacc6e 🚀

--- page-5.js
+++ page-5.js
@@ -1,2 +1,12 @@
-import"./assets/styles-BZUKjEjr.js";console.log("page 05");fetch("https://jsonplaceholder.typicode.com/posts/2").then(o=>o.json()).then(o=>console.log(o));const t={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify({title:"Hello from JS",body:"qweryuiqWAERTY",userId:12})};fetch("https://jsonplaceholder.typicode.com/posts",t).then(o=>{if(!o.ok)throw new Error(o.statusText);return o.json()}).then(o=>console.log(o)).catch(o=>console.log(o));
+import"./assets/styles-BZUKjEjr.js";console.log("page 05");function c(t){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(t)};return fetch("https://jsonplaceholder.typicode.com/posts",o).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}const a=document.querySelector(".js-add"),d=document.querySelector(".js-posts"),r=document.querySelector(".js-form"),i=document.querySelector(".js-error");a.addEventListener("click",l);function l(){r.innerHTML=`
+        <form action="submit" class="js-form-add">
+          <input type="text" name="title">
+          <textarea name="body" cols="30" rows="10"></textarea>
+          <button>Додати пост</button>
+        </form>
+    `;const t=document.querySelector(".js-form-add");t.addEventListener("submit",u),console.log(t)}function u(t){t.preventDefault();const{title:o,body:e}=t.currentTarget.elements,s={title:o.value,body:e.value};c(s).then(n=>{console.log(n),d.insertAdjacentHTML("beforeend",m(n))}).catch(()=>{i.innerHTML="Не можливо додати пост"}).finally(()=>r.innerHTML="")}function m({id:t,title:o,body:e}){return`
+            <li data-id="${t}">
+                <h2>${o}</h2>
+                <p>${e}</p>
+            </li> `}
 //# sourceMappingURL=page-5.js.map
