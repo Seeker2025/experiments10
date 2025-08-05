f52aa24080ea9f46d6d78eabb8ba9a7180e2aa4b
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-08-05 07:15:34 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@1b169858221459dfe5cccaa283989c3fa34fa4b8 🚀

--- page-3.js
+++ page-3.js
@@ -1,2 +1,2 @@
-import"./assets/styles-BZUKjEjr.js";console.log("page 03");function t(r){const e={strawBerry:"strawBerry01",kiwiBerry:"kiwiBerry02",appleBerry:"appleBerry03"};return new Promise((o,a)=>setTimeout(()=>{o(e[r])},1e3))}async function i(){try{const r=await t("kiwiBerry");console.log(r)}catch{console.log("mis")}}i();
+import"./assets/styles-BnGOUoDK.js";console.log("page 03");function t(r){const e={strawBerry:"strawBerry01",kiwiBerry:"kiwiBerry02",appleBerry:"appleBerry03"};return new Promise((o,a)=>setTimeout(()=>{o(e[r])},1e3))}async function i(){try{const r=await t("kiwiBerry");console.log(r)}catch{console.log("mis")}}i();
 //# sourceMappingURL=page-3.js.map
--- page-5.js
+++ page-5.js
@@ -1,11 +1,11 @@
-import"./assets/styles-BZUKjEjr.js";console.log("page 05");function c(t){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(t)};return fetch("https://jsonplaceholder.typicode.com/posts",o).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}const a=document.querySelector(".js-add"),d=document.querySelector(".js-posts"),r=document.querySelector(".js-form"),i=document.querySelector(".js-error");a.addEventListener("click",l);function l(){r.innerHTML=`
-        <form action="submit" class="js-form-add">
+import"./assets/styles-BnGOUoDK.js";console.log("page 05");function a(t){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(t)};return fetch("https://jsonplaceholder.typicode.com/posts",o).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}const d=document.querySelector(".js-add"),i=document.querySelector(".js-posts"),s=document.querySelector(".js-form"),r=document.querySelector(".js-error");d.addEventListener("click",l);function l(){s.innerHTML=`
+        <form action="submit" class="js-form-add form-add">
           <input type="text" name="title">
           <textarea name="body" cols="30" rows="10"></textarea>
           <button>Додати пост</button>
         </form>
-    `;const t=document.querySelector(".js-form-add");t.addEventListener("submit",u),console.log(t)}function u(t){t.preventDefault();const{title:o,body:e}=t.currentTarget.elements,s={title:o.value,body:e.value};c(s).then(n=>{console.log(n),d.insertAdjacentHTML("beforeend",m(n))}).catch(()=>{i.innerHTML="Не можливо додати пост"}).finally(()=>r.innerHTML="")}function m({id:t,title:o,body:e}){return`
-            <li data-id="${t}">
+    `;const t=document.querySelector(".js-form-add");t.addEventListener("submit",u),console.log(t)}function u(t){t.preventDefault();const{title:o,body:e}=t.currentTarget.elements,c={title:o.value,body:e.value};a(c).then(n=>{console.log(n),i.insertAdjacentHTML("beforeend",m(n))}).catch(()=>{r.innerHTML="Не можливо додати пост"}).finally(()=>{s.innerHTML="",setTimeout(()=>{r.innerHTML=""},2e3)})}function m({id:t,title:o,body:e}){return`
+            <li data-id=${t} class="li_box">
                 <h2>${o}</h2>
                 <p>${e}</p>
             </li> `}
