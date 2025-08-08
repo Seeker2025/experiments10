0b314ae6c4cb7c43a9de684c76a959160106a225
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-08-08 17:29:37 +0300

    go on

---
+++ src/js/page07.js
@@ -0,0 +1,7 @@
+console.log('page 07');
+
+console.log(typeof('a'));       /////string
+console.log(typeof(2));         //////number
+console.log(typeof(null));      ////// object
+console.log(typeof(NaN));       ////// number
+console.log(typeof(undefined)); ////// undefined
