e84fe730e17969df716c579e94747b8bbb835d2a
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-08-10 19:30:00 +0300

    go on

--- src/js/page07.js
+++ src/js/page07.js
@@ -1,7 +1,28 @@
 console.log('page 07');
 
-console.log(typeof('a'));       /////string
-console.log(typeof(2));         //////number
-console.log(typeof(null));      ////// object
-console.log(typeof(NaN));       ////// number
-console.log(typeof(undefined)); ////// undefined
+// console.log(typeof('a'));       /////string
+// console.log(typeof(2));         //////number
+// console.log(typeof(null));      ////// object
+// console.log(typeof(NaN));       ////// number
+// console.log(typeof(undefined)); ////// undefined
+
+async function getCapital(){
+    console.log('start');
+    const URL = 'https://restcountries.com/v3.1/name/';
+    const resp = await fetch(`${URL}Ukraine`);
+    const data = await resp.json();
+    // console.log(resp);
+    // console.log(data);
+    console.log('end');
+    return data;
+}
+console.log(getCapital());
+
+// getCapital().then(resp =>{
+//     console.log(resp);
+//      console.log('end_02');
+
+// }).catch(err =>{
+//     console.log('Hi!');
+// })
+
