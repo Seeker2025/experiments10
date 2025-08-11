b461d8e75f2173cb609ba36e9d82f78bc6b21d85
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-08-11 09:57:25 +0000

    Deploying to gh-pages from @ Seeker2025/experiments10@38a76ec262c53bc20c2efcd3e9e191da8cb0b0fe 🚀

--- page-5.js
+++ page-5.js
@@ -1,12 +1,12 @@
-import"./assets/styles-DF3wYhPP.js";console.log("page 05");function a(t){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(t)};return fetch("https://jsonplaceholder.typicode.com/posts",o).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}const d=document.querySelector(".js-add"),i=document.querySelector(".js-posts"),s=document.querySelector(".js-form"),r=document.querySelector(".js-error");d.addEventListener("click",l);function l(){s.innerHTML=`
+import"./assets/styles-DF3wYhPP.js";console.log("page 05");function a(e){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(e)};return fetch("https://jsonplaceholder.typicode.com/posts",o).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})}const l=document.querySelector(".js-add"),i=document.querySelector(".js-posts"),s=document.querySelector(".js-form"),r=document.querySelector(".js-error");l.addEventListener("click",d);function d(){s.innerHTML=`
         <form action="submit" class="js-form-add form-add">
           <input type="text" name="title">
           <textarea name="body" cols="30" rows="10"></textarea>
           <button>Додати пост</button>
         </form>
-    `;const t=document.querySelector(".js-form-add");t.addEventListener("submit",u),console.log(t)}function u(t){t.preventDefault();const{title:o,body:e}=t.currentTarget.elements,c={title:o.value,body:e.value};a(c).then(n=>{console.log(n),i.insertAdjacentHTML("beforeend",m(n))}).catch(()=>{r.innerHTML="Не можливо додати пост"}).finally(()=>{s.innerHTML="",setTimeout(()=>{r.innerHTML=""},2e3)})}function m({id:t,title:o,body:e}){return`
-            <li data-id=${t} class="li_box">
+    `;const e=document.querySelector(".js-form-add");e.addEventListener("submit",u),console.log(e)}function u(e){e.preventDefault(),console.log(e.currentTarget),console.dir(e.currentTarget.elements);const{title:o,body:t}=e.currentTarget.elements,n={title:o.value,body:t.value};if(!(n.title&&n.body)){console.log("fail"),alert("Введіть якийсь текст!"),e.currentTarget.innerHTML=null;return}a(n).then(c=>{i.insertAdjacentHTML("beforeend",m(c))}).catch(()=>{r.innerHTML="Не можливо додати пост"}).finally(()=>{s.innerHTML="",setTimeout(()=>{r.innerHTML=""},2e3)})}function m({id:e,title:o,body:t}){return`
+            <li data-id=${e} class="li_box">
                 <h2>${o}</h2>
-                <p>${e}</p>
+                <p>${t}</p>
             </li> `}
 //# sourceMappingURL=page-5.js.map
--- page-8.js
+++ page-8.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DF3wYhPP.js";console.log("page 08");
+import"./assets/styles-DF3wYhPP.js";console.log("page 08");let o=s=>document.querySelector(s);const n=o(".js-search"),e=o(".js-add"),c=o(".js-list"),l=o(".js-form-container");console.log(n);console.log(e);console.log(c);console.log(l);
 //# sourceMappingURL=page-8.js.map
