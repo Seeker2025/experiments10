0d9a1258b98c2d4f490ba217bd05145def6fd4b5
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-09-13 12:55:24 +0300

    go on

---
+++ src/js/page11.js
@@ -0,0 +1,45 @@
+console.log('page 11');
+
+const refs = {
+    openModalBtn: document.querySelector('[data-action="open-modal"]'),
+    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+    backdrop: document.querySelector('.js-backdrop'),
+};
+
+console.log(refs.openModalBtn);
+console.log(refs.closeModalBtn);
+console.log(refs.backdrop);
+
+refs.openModalBtn.addEventListener('click', onOpenModal)
+refs.closeModalBtn.addEventListener('click', onCloseModal)
+refs.backdrop.addEventListener('click', onBackdropClick)
+
+
+
+function onOpenModal(){
+    //////we hang the listener on Window only when we open the modal
+    window.addEventListener('keydown', onEscKeyPress);
+    document.body.classList.add('show-modal');
+}
+
+function onCloseModal(){
+    //////we remove listener from windows when close modal
+    window.removeEventListener('keydown', onEscKeyPress);
+    document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(event){
+    console.log(event.currentTarget);
+    console.log(event.target);
+
+    if(event.currentTarget === event.target){
+        onCloseModal();
+    }
+}
+
+function onEscKeyPress(event){
+    if(event.code === 'Escape'){
+        onCloseModal();
+    }
+    console.log(event);
+}
